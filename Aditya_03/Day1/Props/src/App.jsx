dad16bd935056ebd4d0b8dfebf151276370d52ef
aditya <sahooadityaranjan2024@gmail.com>
2026-01-06 15:35:08 +0530

    Props

---
+++ Aditya_03/Day1/Props/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import About from './About.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      hello world
+      <About a={20}/>
+    </>
+  )
+}
+
+export default App
