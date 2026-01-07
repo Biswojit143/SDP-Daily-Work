a8f773b6a2eb248b13d459fee7ed4c3b17feeaaf
aditya <sahooadityaranjan2024@gmail.com>
2026-01-07 15:29:59 +0530

    Usecontext

--- Aditya_03/Day2/usestate-React/src/App.jsx
+++ Aditya_03/Day2/usestate-React/src/App.jsx
@@ -1,22 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createContext } from 'react'
 import './App.css'
-import Home from './Home'
+import Child1 from './Child1';
+
+let nameContext = createContext();
+let themeContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name,setname]=useState("");
-  const [obj,setobj]=useState(null);
-
-  let handelText=()=>{
-  setname("Aditya");
-}
+ 
   return (
     <>
-      <Home />
+      Today we are going  to learn then useContext
+      {
+      //   The steps to create the context object.
+
+      // => Use the createContext method to create the context object.
+      // => use the context provider in the Component
+      // add the value
+      // => export the context object.
+
+      // How to acess the context object in the other component.
+
+      // => use the useContext hook to access the context object to get the context data.
+      }
+      <nameContext.Provider value={"Hello from context"}>
+        <themeContext.Provider value={"dark"}>
+          <Child1 />
+        </themeContext.Provider>
+      </nameContext.Provider>
     </>
   )
 }
 
+export {nameContext, themeContext}
 export default App
